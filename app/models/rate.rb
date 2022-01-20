6574d04e678be3f0f374e7369155bd3f5cf1c0b1
HARSHITHA <harshitha.r@ia.ooo>
2022-01-20 20:23:08 +0530

    test..

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,6 +1,8 @@
 class Rate < ApplicationRecord
+
   validates :rate, :base_currency_id, :currency_id, presence: true
   validates :rate, :numericality => { :greater_than => 0}
   belongs_to :base_currency, class_name: "Currency"
   belongs_to :currency
+
 end
