305807e4c7cdd40bf0b6cf65c678ea2b02eed978
HARSHITHA <harshitha.r@ia.ooo>
2022-01-21 21:08:42 +0530

    code refatoring...

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -3,4 +3,25 @@ class Rate < ApplicationRecord
   validates :rate, :numericality => { :greater_than => 0 }
   belongs_to :base_currency, class_name: "Currency"
   belongs_to :currency
+
+
+  def self.update_rate(response, currency)
+    rate = currency.rate
+    rate.update(rate: response['rates'][currency.code])
+  end
+
+  def self.make_rate_api_call(currency1, currency2)
+    response = HTTParty.get("#{ENV["rate_domain"]}?key=#{ENV["api_key"]}")
+    update_rate(response, currency1)
+    update_rate(response, currency2)
+  end
+
+  def self.check_and_update_rate(from_currency,to_currency)
+    from_rate_diff = (Time.current - from_currency.rate.updated_at) / 60
+    to_rate_diff = (Time.current - to_currency.rate.updated_at) / 60
+
+    if from_rate_diff > 30 || to_rate_diff > 30
+      Rate.make_rate_api_call(from_currency, to_currency)
+    end
+  end
 end
--- lib/tasks/feed_rate.rake
+++ lib/tasks/feed_rate.rake
@@ -1,4 +1,20 @@
 task feed_rate: :environment do
-	Currency.create_or_update_rates
+	response = HTTParty.get("#{ENV["rate_domain"]}?key=#{ENV["api_key"]}")
+	base_currency = Currency.find_by_code('USD')
+	if base_currency.nil?
+		puts "base_currency is not present"
+	else
+		data = response['rates']
+		data.each do |currency_code,rate|
+			currency = Currency.find_by_code(currency_code)
+			db_rate = Rate.find_by({base_currency_id: base_currency.id, currency_id: currency.id})
+			if db_rate.present?
+				db_rate.rate = rate
+				db_rate.save
+			else
+				Rate.create({base_currency_id: base_currency.id, currency_id: currency.id, rate: rate})
+			end
+		end
+	end
 end
 
